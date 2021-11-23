1d3b0bcf3656deec32eb7ddc380b36a685f4e9a1
Inspiron 5515 <Inspiron 5515@DESKTOP-B68DCDH>
2021-11-23 12:00:15 +0700

    Add project files.

---
+++ quanly.form/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace quanly.form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_magiay = new System.Windows.Forms.TextBox();
+            this.tb_tengiay = new System.Windows.Forms.TextBox();
+            this.tb_size = new System.Windows.Forms.TextBox();
+            this.tb_soluong = new System.Windows.Forms.TextBox();
+            this.tb_mausac = new System.Windows.Forms.TextBox();
+            this.tb_dongia = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 178);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tb_dongia);
+            this.groupBox2.Controls.Add(this.tb_mausac);
+            this.groupBox2.Controls.Add(this.tb_soluong);
+            this.groupBox2.Controls.Add(this.tb_size);
+            this.groupBox2.Controls.Add(this.tb_tengiay);
+            this.groupBox2.Controls.Add(this.tb_magiay);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 210);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(548, 203);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin chi tiết";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(764, 151);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Giày";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Size";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên Giày";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số lượng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Màu sắc";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(216, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Đơn giá bán";
+            // 
+            // tb_magiay
+            // 
+            this.tb_magiay.Location = new System.Drawing.Point(88, 25);
+            this.tb_magiay.Name = "tb_magiay";
+            this.tb_magiay.Size = new System.Drawing.Size(100, 22);
+            this.tb_magiay.TabIndex = 6;
+            // 
+            // tb_tengiay
+            // 
+            this.tb_tengiay.Location = new System.Drawing.Point(88, 68);
+            this.tb_tengiay.Name = "tb_tengiay";
+            this.tb_tengiay.Size = new System.Drawing.Size(100, 22);
+            this.tb_tengiay.TabIndex = 7;
+            // 
+            // tb_size
+            // 
+            this.tb_size.Location = new System.Drawing.Point(88, 142);
+            this.tb_size.Name = "tb_size";
+            this.tb_size.Size = new System.Drawing.Size(100, 22);
+            this.tb_size.TabIndex = 8;
+            // 
+            // tb_soluong
+            // 
+            this.tb_soluong.Location = new System.Drawing.Point(317, 25);
+            this.tb_soluong.Name = "tb_soluong";
+            this.tb_soluong.Size = new System.Drawing.Size(100, 22);
+            this.tb_soluong.TabIndex = 9;
+            // 
+            // tb_mausac
+            // 
+            this.tb_mausac.Location = new System.Drawing.Point(317, 68);
+            this.tb_mausac.Name = "tb_mausac";
+            this.tb_mausac.Size = new System.Drawing.Size(100, 22);
+            this.tb_mausac.TabIndex = 10;
+            // 
+            // tb_dongia
+            // 
+            this.tb_dongia.Location = new System.Drawing.Point(317, 142);
+            this.tb_dongia.Name = "tb_dongia";
+            this.tb_dongia.Size = new System.Drawing.Size(100, 22);
+            this.tb_dongia.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(646, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Thêm ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(646, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Xóa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(646, 325);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Sửa";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(646, 383);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 30);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Khởi tạo";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_dongia;
+        private System.Windows.Forms.TextBox tb_mausac;
+        private System.Windows.Forms.TextBox tb_soluong;
+        private System.Windows.Forms.TextBox tb_size;
+        private System.Windows.Forms.TextBox tb_tengiay;
+        private System.Windows.Forms.TextBox tb_magiay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ quanly.form/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace quanly.form
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        string str = @"Data Source=DESKTOP-B68DCDH\SQLEXPRESS;Initial Catalog=SHOES10;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+
+        void loaddata()
+        {
+            command = connection.CreateCommand();
+            command.CommandText = " Select * from Mat_Hang";
+            adapter.SelectCommand = command;
+            table.Clear();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(str);
+            connection.Open();
+            loaddata();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dataGridView1.CurrentRow.Index;
+            tb_magiay.Text = dataGridView1.Rows[i].Cells[0].Value.ToString();
+            tb_tengiay.Text = dataGridView1.Rows[i].Cells[1].Value.ToString();
+            tb_size.Text = dataGridView1.Rows[i].Cells[4].Value.ToString();
+            tb_soluong.Text = dataGridView1.Rows[i].Cells[2].Value.ToString();
+            tb_mausac.Text = dataGridView1.Rows[i].Cells[3].Value.ToString();
+            tb_dongia.Text = dataGridView1.Rows[i].Cells[5].Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            command.Connection.CreateCommand();
+            command.CommandText = "insert into Mat_Hang values ('" + tb_magiay.Text + "','" + tb_tengiay.Text + "','" + tb_soluong.Text + "','" + tb_mausac.Text + "','" + tb_size.Text + "','" + tb_dongia + "')";
+            command.ExecuteNonQuery();
+            loaddata();
+        }
+    }
+}
